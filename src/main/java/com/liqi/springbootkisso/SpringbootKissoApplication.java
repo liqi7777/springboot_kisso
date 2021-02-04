c78c2baf2411e7584d59020f22840f2db807f8f1
liqi <sky.li>
2021-02-04 16:32:07 +0800

    springboot-kisso init commit

---
+++ src/main/java/com/liqi/springbootkisso/SpringbootKissoApplication.java
@@ -0,0 +1,13 @@
+package com.liqi.springbootkisso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKissoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootKissoApplication.class, args);
+    }
+
+}

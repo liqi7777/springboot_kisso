4651e482523cad2f553b5a8d2122861309aff119
liqi <sky.li>
2021-02-04 17:17:37 +0800

    springboot-kisso dev first

--- src/main/java/com/liqi/springbootkisso/SpringbootKissoApplication.java
+++ src/main/java/com/liqi/springbootkisso/SpringbootKissoApplication.java
@@ -1,14 +1,18 @@
 package com.liqi.springbootkisso;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class SpringbootKissoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootKissoApplication.class, args);
 
+
+        log.info("dev-test");
     }
 
 }
